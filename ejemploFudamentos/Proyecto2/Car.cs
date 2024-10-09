de0bf6241d394ed06004a7a50ba1b87220fd48fb
1002820487 <manuel.erazo@estudiante.fup.edu.co>
2024-10-09 15:41:38 -0500

    ejemplo

---
+++ ejemploFudamentos/Proyecto2/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto2
+{
+    public class Car
+    {
+        string color;
+        int cilindraje;
+        int modelo;
+
+        public string stop()
+        {
+            string msj = "El carro esta detenido";
+            return msj; 
+        }
+    }
+}
